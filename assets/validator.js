09d57cae44f815a71bdf8d30430ff5b33ceea1ca
András Kamocsai <kamo.andras@gmail.com>
2017-11-17 10:47:01 +0100

    feat(exam): search endpoint created

---
+++ assets/validator.js
@@ -0,0 +1,27 @@
+// let validator = (function () {
+//     let inputValidator = function (input) {
+//         let result;
+//         if (input.indexOf('@') === true) {
+//         result = false;
+//         } else {
+//         result = true;
+//         }
+//     }
+//     let isValid = function (data) {
+//         var result;
+//         if (checkTextFunc > 3 && checkScaleFunc >= 10 && checkEmailFunc) {
+//           result = true;
+//         } else {
+//           result = false;
+//         }
+//         return result;
+//       };
+    
+//       return {
+//         validator: isValid
+//       };
+// })();
+
+let validator = () => true;
+
+module.exports = validator;
